3c1d69aee80a09c06fbfd2c98829d6853913dd72
Nino Joy <ninojoy1234@gmail.com>
2021-11-18 11:32:03 +0530

    data fetch - integrated async storage

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text, View, FlatList} from 'react-native';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const BASE_URL = 'http://www.mocky.io/v2/5d565297300000680030a986';
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const renderItem = ({item}) => (
+    <View>
+      <Text>{item.name}</Text>
+    </View>
+  );
+
+  const fetchData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem('@data');
+      if (jsonValue != null) {
+        setData(JSON.parse(jsonValue));
+      } else {
+        const response = await fetch(BASE_URL);
+        const json = await response.json();
+        const jsonValue = JSON.stringify(json);
+        await AsyncStorage.setItem('@data', jsonValue);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.heading}>Employee Directory</Text>
+      <FlatList
+        data={data}
+        contentContainerStyle={{
+          marginTop: 20,
+        }}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
@@ -14,7 +54,11 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  heading: {
+    fontSize: 30,
+    alignSelf: 'center',
+    fontWeight: 'bold',
   },
 });
