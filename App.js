561e3b7b415916dd25d51eed55153c92cf428f86
Nino Joy <ninojoy1234@gmail.com>
2021-11-18 10:44:12 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
